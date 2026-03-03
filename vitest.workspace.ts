90d5d3025128223157c8951c2ca88aa8ff3d7863
Jacob Freck <jafreck@outlook.com>
2026-03-03 10:13:46 -0800

    fix(issue-22): address integration issues (#22)

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -1,27 +1,7 @@
 import { defineWorkspace } from 'vitest/config';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineWorkspace([
-  {
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, './packages/web/src'),
-      },
-    },
-    esbuild: {
-      jsx: 'automatic',
-      jsxImportSource: 'react',
-    },
-    test: {
-      name: 'web',
-      include: ['packages/web/tests/**/*.test.{ts,tsx}'],
-      environment: 'jsdom',
-      passWithNoTests: true,
-    },
-  },
+  'packages/web/vitest.config.ts',
   {
     test: {
       name: 'non-web',
